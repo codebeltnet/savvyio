82f4b6684ffb1fe30323edc7cedf53bf9d628576
gimlichael <michael@geekle.io>
2022-01-08 17:40:36 +0100

    Domain

--- src/Savvyio/Domain/IDomainEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Savvyio.Domain
-{
-    /// <summary>
-    /// A marker interface that specifies something that happened in the domain that you want other parts of the same domain (in-process/inner-application) to be made aware of.
-    /// </summary>
-    public interface IDomainEvent : IEvent
-    {
-    }
-}
