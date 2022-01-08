a3cc3f994657774090d2561e08f725a449ef0bb5
gimlichael <michael@geekle.io>
2022-01-08 03:26:54 +0100

    Mediator

---
+++ src/Savvyio.Extensions/IMediator.cs
@@ -0,0 +1,18 @@
+﻿using Savvyio.Commands;
+using Savvyio.Domain;
+using Savvyio.EventDriven;
+using Savvyio.Queries;
+
+namespace Savvyio.Extensions
+{
+    /// <summary>
+    /// Defines a mediator to encapsulate requests (Fire-and-Forget/In-Only) and request/response (Request-Reply/In-Out) patterns.
+    /// </summary>
+    /// <seealso cref="ICommandDispatcher" />
+    /// <seealso cref="IDomainEventDispatcher" />
+    /// <seealso cref="IIntegrationEventDispatcher" />
+    /// <seealso cref="IQueryDispatcher" />
+    public interface IMediator : ICommandDispatcher, IDomainEventDispatcher, IIntegrationEventDispatcher, IQueryDispatcher
+    {
+    }
+}
---
+++ src/Savvyio.Extensions/SavvyioOptionsExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Savvyio.Commands;
+using Savvyio.Domain;
+using Savvyio.EventDriven;
+using Savvyio.Queries;
+
+namespace Savvyio.Extensions
+{
+    /// <summary>
+    /// Extension methods for the <see cref="SavvyioOptions"/> class.
+    /// </summary>
+    public static class SavvyioOptionsExtensions
+    {
+        /// <summary>
+        /// Adds an implementation of the <see cref="IMediator"/> interface.
+        /// </summary>
+        /// <typeparam name="TImplementation">The type of the implementation to use.</typeparam>
+        /// <param name="options">The <see cref="SavvyioOptions"/> to extend.</param>
+        /// <returns>A reference to <paramref name="options"/> after the operation has completed.</returns>
+        public static SavvyioOptions AddMediator<TImplementation>(this SavvyioOptions options) where TImplementation : class, IMediator
+        {
+            options.AddDispatcher<IMediator, TImplementation>();
+            options.AddDispatcher<ICommandDispatcher, TImplementation>();
+            options.AddDispatcher<IDomainEventDispatcher, TImplementation>();
+            options.AddDispatcher<IIntegrationEventDispatcher, TImplementation>();
+            options.AddDispatcher<IQueryDispatcher, TImplementation>();
+            return options;
+        }
+    }
+}
