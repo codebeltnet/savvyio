893a0086823429dcff4329c8b32ac5a675cc0479
gimlichael <michael@geekle.io>
2022-01-09 13:03:20 +0100

    Messaging - early foresee

--- src/Savvyio.EventDriven.Messaging/IMessage.cs
+++ src/Savvyio.EventDriven.Messaging/IMessage.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Savvyio.Messaging
+﻿namespace Savvyio.EventDriven.Messaging
 {
     public interface IMessage<out TData>
     {
