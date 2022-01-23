a1fb543cecb6a82963b6585580d864cb91165ec3
gimlichael <michael@geekle.io>
2022-01-23 20:47:58 +0100

    Various

---
+++ src/Savvyio.Domain.EventSourcing/Storage/IEventSourcingRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Cuemon.Threading;
+using Savvyio.Storage;
+
+namespace Savvyio.Domain.EventSourcing.Storage
+{
+    /// <summary>
+    /// Defines a repository complying to the Event Sourcing concept.
+    /// </summary>
+    /// <typeparam name="TAggregate">The type of the model.</typeparam>
+    /// <typeparam name="TKey">The type of the key that uniquely identifies the model.</typeparam>
+    public interface IEventSourcingRepository<TAggregate, TKey> : IWritableRepository<TAggregate, TKey> where TAggregate : class, ITracedAggregateRoot<TKey>
+    {
+        /// <summary>
+        /// Reads a stream of events asynchronous that rehydrates the state of a model.
+        /// </summary>
+        /// <param name="id">The identifier of the model.</param>
+        /// <param name="setup">The <see cref="AsyncOptions"/> which may be configured.</param>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous operation. The task result contains the rehydrated model.</returns>
+        Task<TAggregate> GetByIdAsync(TKey id, Action<AsyncOptions> setup = null);
+    }
+}
--- src/Savvyio.Domain.EventSourcing/TracedAggregateRoot.cs
+++ src/Savvyio.Domain.EventSourcing/TracedAggregateRoot.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Savvyio.Handlers;
 
 namespace Savvyio.Domain.EventSourcing
 {
