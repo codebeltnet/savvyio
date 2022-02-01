a94d928a49ba8bdd069b15ce572c6581201b43ee
gimlichael <michael@geekle.io>
2022-02-01 05:44:14 +0100

    Introduction of ISearchableRepository due to Event Sourcing

--- src/Savvyio.Extensions.DependencyInjection/Storage/IPersistentRepository.cs
+++ src/Savvyio.Extensions.DependencyInjection/Storage/IPersistentRepository.cs
@@ -10,8 +10,9 @@ namespace Savvyio.Extensions.DependencyInjection.Storage
     /// <typeparam name="TMarker">The type used to mark the implementation that this repository represents. Optimized for Microsoft Dependency Injection.</typeparam>
     /// <seealso cref="IWritableRepository{TEntity,TKey,TMarker}"/>
     /// <seealso cref="IReadableRepository{TEntity,TKey,TMarker}"/>
+    /// <seealso cref="ISearchableRepository{TEntity,TKey,TMarker}"/>
     /// <seealso cref="IDeletableRepository{TEntity,TKey,TMarker}"/>
-    public interface IPersistentRepository<TEntity, TKey, TMarker> : IPersistentRepository<TEntity, TKey>, IWritableRepository<TEntity, TKey, TMarker>, IReadableRepository<TEntity, TKey, TMarker>, IDeletableRepository<TEntity, TKey, TMarker> where TEntity : class, IIdentity<TKey>
+    public interface IPersistentRepository<TEntity, TKey, TMarker> : IPersistentRepository<TEntity, TKey>, IWritableRepository<TEntity, TKey, TMarker>, IReadableRepository<TEntity, TKey, TMarker>, ISearchableRepository<TEntity, TKey, TMarker>, IDeletableRepository<TEntity, TKey, TMarker> where TEntity : class, IIdentity<TKey>
     {
     }
 }
