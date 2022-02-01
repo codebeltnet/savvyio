983e46d0486ad402a57939fa5e70a16e1a10c4bc
gimlichael <michael@geekle.io>
2022-02-01 05:43:33 +0100

    Two new repository interfaces

---
+++ src/Savvyio.Extensions.DependencyInjection.Domain/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Major Code Smell", "S2436:Types and methods should not have too many generic parameters", Justification = "By design to support service-to-multiple implementations in MS DI.", Scope = "type", Target = "~T:Savvyio.Extensions.DependencyInjection.Domain.IAggregateRepository`3")]
+[assembly: SuppressMessage("Major Code Smell", "S2436:Types and methods should not have too many generic parameters", Justification = "By design to support service-to-multiple implementations in MS DI.", Scope = "type", Target = "~T:Savvyio.Extensions.DependencyInjection.Domain.EventSourcing.ITracedAggregateRepository`3")]
---
+++ src/Savvyio.Extensions.DependencyInjection.Domain/IAggregateRepository.cs
@@ -0,0 +1,17 @@
+﻿using Savvyio.Domain;
+using Savvyio.Extensions.DependencyInjection.Storage;
+
+namespace Savvyio.Extensions.DependencyInjection.Domain
+{
+    /// <summary>
+    /// Defines a generic way to support multiple implementations of persistent repositories (CRUD) that is optimized for Domain Driven Design.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the entity.</typeparam>
+    /// <typeparam name="TKey">The type of the key that uniquely identifies the entity.</typeparam>
+    /// <typeparam name="TMarker">The type used to mark the implementation that this repository represents. Optimized for Microsoft Dependency Injection.</typeparam>
+    /// <seealso cref="IAggregateRepository{TEntity,TKey}"/>
+    /// <seealso cref="IPersistentRepository{TEntity,TKey,TMarker}"/>
+    public interface IAggregateRepository<TEntity, TKey, TMarker> : IAggregateRepository<TEntity, TKey>, IPersistentRepository<TEntity, TKey, TMarker> where TEntity : class, IAggregateRoot<IDomainEvent, TKey>
+    {
+    }
+}
