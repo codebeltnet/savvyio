7ddd830f92ab1dfacc3fd28c4bb4e2b2ae6311ad
gimlichael <michael@geekle.io>
2022-03-21 07:27:04 +0100

    Typo

--- src/Savvyio.Queries/IQueryHandler.cs
+++ src/Savvyio.Queries/IQueryHandler.cs
@@ -3,7 +3,7 @@
 namespace Savvyio.Queries
 {
     /// <summary>
-    /// Specifies a handler resposible for objects that implements the <see cref="IQuery"/> interface.
+    /// Specifies a handler responsible for objects that implements the <see cref="IQuery"/> interface.
     /// </summary>
     /// <seealso cref="IRequestReplyHandler{TRequest}" />
     public interface IQueryHandler : IRequestReplyHandler<IQuery>
