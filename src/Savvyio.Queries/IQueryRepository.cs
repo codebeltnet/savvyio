6516f48edd4174be2e5e9b6e135e1616f32a7db7
gimlichael <michael@geekle.io>
2022-05-08 04:28:58 +0200

    Obsolete

--- src/Savvyio.Queries/IQueryRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Cuemon.Threading;
-using Savvyio.Data;
-
-namespace Savvyio.Queries
-{
-    public interface IQueryRepository<TProjection> : IPersistentDataAccessObject<TProjection, AsyncOptions> where TProjection : class
-    {
-    }
-}
