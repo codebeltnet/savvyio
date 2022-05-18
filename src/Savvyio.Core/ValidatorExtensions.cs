1caa6494e5290816cc60a73f2b2cfc8ed8991baf
gimlichael <michael@geekle.io>
2022-05-18 18:30:11 +0200

    Added doc

--- src/Savvyio.Core/ValidatorExtensions.cs
+++ src/Savvyio.Core/ValidatorExtensions.cs
@@ -1,14 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Cuemon;
 
 namespace Savvyio
 {
+    /// <summary>
+    /// Extension methods for the <see cref="Validator"/> class.
+    /// </summary>
     public static class ValidatorExtensions
     {
+        /// <summary>
+        /// Provides a convenient way to validate a parameter while returning the specified <paramref name="value"/> unaltered.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to evaluate.</typeparam>
+        /// <param name="_">The <see cref="Validator"/> to extend.</param>
+        /// <param name="value">The value to be evaluated.</param>
+        /// <param name="validator">The delegate that must throw an <see cref="Exception"/> if the specified <paramref name="value"/> is not valid.</param>
+        /// <returns>The specified <paramref name="value"/> unaltered.</returns>
         public static T InvalidState<T>(this Validator _, T value, Action<T> validator)
         {
             validator(value);
