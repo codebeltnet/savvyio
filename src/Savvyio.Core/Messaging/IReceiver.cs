39d79b235c51fa7bf3888414acad3f37cdcc02a9
gimlichael <michael@geekle.io>
2022-11-28 02:18:15 +0100

    Typo

--- src/Savvyio.Core/Messaging/IReceiver.cs
+++ src/Savvyio.Core/Messaging/IReceiver.cs
@@ -10,7 +10,7 @@ namespace Savvyio.Messaging
     public interface IReceiver<TRequest> where TRequest : IRequest
     {
         /// <summary>
-        /// Receive one or more command(s) asynchronous using Point-to-Point Channel/P2P MEP.
+        /// Receive one or more message(s) asynchronous using Point-to-Point Channel/P2P MEP.
         /// </summary>
         /// <param name="setup">The <see cref="ReceiveAsyncOptions" /> which may be configured.</param>
         /// <returns>A task that represents the asynchronous operation. The task result contains a sequence of <see cref="IMessage{T}"/> whose generic type argument is <see cref="IRequest"/>.</returns>
