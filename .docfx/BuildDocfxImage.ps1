5e188ab87c0782a725b81721a331a5a9ca334566
Michael Mortensen <michael@geekle.io>
2024-06-08 22:58:10 +0200

    :hammer: DocFX optimization

--- .docfx/BuildDocfxImage.ps1
+++ .docfx/BuildDocfxImage.ps1
@@ -1,8 +1,4 @@
-﻿$version = minver
+﻿$version = minver -i
 docfx metadata docfx.json
-curl -sSL --output cuemon-xrefmap.yml https://docs.cuemon.net/xrefmap.yml
-(echo "baseUrl: https://docs.cuemon.net/" && cat cuemon-xrefmap.yml) > cuemon-xrefmap.lock && mv cuemon-xrefmap.lock xrefmaps/cuemon-xrefmap.yml
-docker build -t savvyio-docfx:$version -f Dockerfile.docfx . # --progress plain
+docker buildx build -t savvyio-docfx:$version --platform linux/arm64,linux/amd64 --load -f Dockerfile.docfx .
 get-childItem -recurse -path api -include *.yml, .manifest | remove-item
-get-childItem -recurse -path xrefmaps -include *.yml, .manifest | remove-item
-remove-Item cuemon-xrefmap.yml
--- .docfx/PublishDocfxImage.ps1
+++ .docfx/PublishDocfxImage.ps1
@@ -1,3 +1,3 @@
-$version = minver
+$version = minver -i
 docker tag savvyio-docfx:$version jcr.codebelt.net/geekle/savvyio-docfx:$version
 docker push jcr.codebelt.net/geekle/savvyio-docfx:$version
