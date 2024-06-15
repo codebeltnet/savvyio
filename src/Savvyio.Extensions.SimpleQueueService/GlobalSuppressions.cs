c6bd64147ac826211628fe6027e894f004b0d584
Michael Mortensen <michael@geekle.io>
2024-06-15 18:27:10 +0200

    :rotating_light: justified sonarcloud warnings

---
+++ src/Savvyio.Extensions.SimpleQueueService/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Major Code Smell", "S6966:Awaitable method should be used", Justification = "No need for async in this context (small payload).", Scope = "member", Target = "~M:Savvyio.Extensions.SimpleQueueService.AmazonMessage`1.ReceiveMessagesAsync(Amazon.SQS.IAmazonSQS,System.Threading.CancellationToken)~System.Collections.Generic.IAsyncEnumerable{Savvyio.Messaging.IMessage{`0}}")]
