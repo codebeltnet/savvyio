6c07720fb871871a4c3d1cb4d1af444b825e21a2
Michael Mortensen <michael@geekle.io>
2024-09-01 20:42:16 +0200

    :wrench: updated DocFX

--- .docfx/PublishDocfxImage.ps1
+++ .docfx/PublishDocfxImage.ps1
@@ -1,3 +1,3 @@
-$version = minver -i
+$version = minver -i -t v -v w
 docker tag savvyio-docfx:$version jcr.codebelt.net/geekle/savvyio-docfx:$version
 docker push jcr.codebelt.net/geekle/savvyio-docfx:$version
