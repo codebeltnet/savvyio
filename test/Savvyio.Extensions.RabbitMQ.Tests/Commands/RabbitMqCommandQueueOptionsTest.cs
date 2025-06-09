00690af58fc5160a123fa3bdb754388ff2d4d089
Michael Mortensen <michael@geekle.io>
2025-06-09 20:43:32 +0200

    :white_check_mark: complementary unit tests

---
+++ test/Savvyio.Extensions.RabbitMQ.Tests/Commands/RabbitMqCommandQueueOptionsTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xunit;
+
+namespace Savvyio.Extensions.RabbitMQ.Commands
+{
+    public class RabbitMqCommandQueueOptionsTest
+    {
+        [Fact]
+        public void Constructor_Should_Set_Defaults()
+        {
+            var options = new RabbitMqCommandQueueOptions();
+
+            Assert.Null(options.QueueName);
+            Assert.False(options.AutoAcknowledge);
+            Assert.NotNull(options.AmqpUrl); // Inherited from RabbitMqMessageOptions
+        }
+
+        [Fact]
+        public void ValidateOptions_Should_Throw_When_QueueName_Is_Null_Or_Empty()
+        {
+            var options = new RabbitMqCommandQueueOptions();
+
+            Assert.Throws<InvalidOperationException>(() => options.ValidateOptions());
+
+            options.QueueName = "";
+            Assert.Throws<InvalidOperationException>(() => options.ValidateOptions());
+        }
+
+        [Fact]
+        public void ValidateOptions_Should_Not_Throw_When_QueueName_Is_Set()
+        {
+            var options = new RabbitMqCommandQueueOptions
+            {
+                QueueName = "test-queue"
+            };
+
+            var exception = Record.Exception(() => options.ValidateOptions());
+            Assert.Null(exception);
+        }
+    }
+}
---
+++ test/Savvyio.Extensions.RabbitMQ.Tests/RabbitMqMessageOptionsTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xunit;
+
+namespace Savvyio.Extensions.RabbitMQ
+{
+    public class RabbitMqMessageOptionsTest
+    {
+        [Fact]
+        public void Constructor_Should_Set_Default_AmqpUrl()
+        {
+            var options = new RabbitMqMessageOptions();
+
+            Assert.NotNull(options.AmqpUrl);
+            Assert.Equal(new Uri("amqp://localhost:5672"), options.AmqpUrl);
+        }
+
+        [Fact]
+        public void ValidateOptions_Should_Throw_When_AmqpUrl_Is_Null()
+        {
+            var options = new RabbitMqMessageOptions
+            {
+                AmqpUrl = null
+            };
+
+            Assert.Throws<InvalidOperationException>(() => options.ValidateOptions());
+        }
+
+        [Fact]
+        public void ValidateOptions_Should_Not_Throw_When_AmqpUrl_Is_Set()
+        {
+            var options = new RabbitMqMessageOptions
+            {
+                AmqpUrl = new Uri("amqp://localhost:5672")
+            };
+
+            var exception = Record.Exception(() => options.ValidateOptions());
+            Assert.Null(exception);
+        }
+    }
+}
