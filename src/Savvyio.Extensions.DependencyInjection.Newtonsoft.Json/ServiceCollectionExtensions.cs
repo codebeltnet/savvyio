da3a8d9cc96fb7ef735bbb89b6ef2357c34e6b72
Michael Mortensen <michael@geekle.io>
2025-08-29 22:27:35 +0200

    :sparkles: support for System.Text.Json and Newtonsof.Json with Microsoft Dependency Injection

---
+++ src/Savvyio.Extensions.DependencyInjection.Newtonsoft.Json/ServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Codebelt.Extensions.Newtonsoft.Json.Formatters;
+using Cuemon;
+using Cuemon.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Savvyio.Extensions.Newtonsoft.Json;
+
+namespace Savvyio.Extensions.DependencyInjection.Newtonsoft.Json
+{
+    /// <summary>
+    /// Extension methods for the <see cref="IServiceCollection"/> interface.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds an <see cref="NewtonsoftJsonMarshaller" /> implementation to the specified <see cref="IServiceCollection" />.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to extend.</param>
+        /// <param name="jsonSetup">The <see cref="NewtonsoftJsonFormatterOptions" /> which may be configured. Default is optimized for messaging.</param>
+        /// <param name="serviceSetup">The <see cref="ServiceOptions" /> which may be configured. Default is <see cref="ServiceLifetime.Singleton"/>.</param>
+        /// <returns>A reference to <paramref name="services"/> so that additional calls can be chained.</returns>
+        /// <remarks>The implementation will be type forwarded accordingly.</remarks>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="services"/> cannot be null.
+        /// </exception>
+        public static IServiceCollection AddNewtonsoftJsonMarshaller(this IServiceCollection services, Action<NewtonsoftJsonFormatterOptions> jsonSetup = null, Action<ServiceOptions> serviceSetup = null)
+        {
+            Validator.ThrowIfNull(services);
+            return services
+                .AddMarshaller<NewtonsoftJsonMarshaller>(serviceSetup)
+                .AddConfiguredOptions(jsonSetup ?? (o => o.Settings.Formatting = Formatting.None));
+        }
+    }
+}
---
+++ src/Savvyio.Extensions.DependencyInjection.Text.Json/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Cuemon.Extensions.DependencyInjection;
+using Cuemon.Extensions.Text.Json.Formatters;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Cuemon;
+using Savvyio.Extensions.Text.Json;
+
+namespace Savvyio.Extensions.DependencyInjection.Text.Json
+{
+    /// <summary>
+    /// Extension methods for the <see cref="IServiceCollection"/> interface.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds an <see cref="JsonMarshaller" /> implementation to the specified <see cref="IServiceCollection" />.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to extend.</param>
+        /// <param name="jsonSetup">The <see cref="JsonFormatterOptions" /> which may be configured. Default is optimized for messaging.</param>
+        /// <param name="serviceSetup">The <see cref="ServiceOptions" /> which may be configured. Default is <see cref="ServiceLifetime.Singleton"/>.</param>
+        /// <returns>A reference to <paramref name="services"/> so that additional calls can be chained.</returns>
+        /// <remarks>The implementation will be type forwarded accordingly.</remarks>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="services"/> cannot be null.
+        /// </exception>
+        public static IServiceCollection AddJsonMarshaller(this IServiceCollection services, Action<JsonFormatterOptions> jsonSetup = null, Action<ServiceOptions> serviceSetup = null)
+        {
+            Validator.ThrowIfNull(services);
+            return services
+                .AddMarshaller<JsonMarshaller>(serviceSetup)
+                .AddConfiguredOptions(jsonSetup ?? (o => o.Settings.WriteIndented = false));
+        }
+    }
+}
